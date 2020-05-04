2ed5a1c0eca3ec6a0469b22d4cdd8ad03862ee8b
dhari <dharinihvaghela@gmail.com>
2020-05-05 01:44:15 +0530

    bacis structure and scss with class modules

---
+++ src/auth/Auth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+    render() {
+        return (
+            <div>
+                Auth Component working!!
+            </div>
+        );
+    }
+}
+
+export default Auth;
