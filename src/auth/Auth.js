1fa6afaa7077e82282253b1d5f4d472dfafb56b2
dhari <dharinihvaghela@gmail.com>
2020-05-05 02:07:26 +0530

    angular material setup

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class Auth extends Component {
     render() {
         return (
             <div>
                 Auth Component working!!
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                >Sign In</Button>
             </div>
         );
     }
