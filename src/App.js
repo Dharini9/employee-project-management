139f403403145497b65ed9d0ad55e6651104b50e
Dharini <dharini.vaghela@azilen.com>
2020-07-01 01:28:37 +0530

    link changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'antd/dist/antd.css';
 import Employee from './employee/Employees';
 
 const client = new ApolloClient({
-  uri: "http://192.168.4.31:5000/graphql"
+  uri: "http://localhost:5000/graphql"
 });
 
 function App() {
