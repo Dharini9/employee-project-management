f0d9789bbae753bca83d7c9cbccd9351918d8c1d
Dharini <dharini.vaghela@azilen.com>
2020-07-01 01:26:20 +0530

    ant form integration and add mutation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import 'antd/dist/antd.css';
+import Employee from './employee/Employees';
+
+const client = new ApolloClient({
+  uri: "http://192.168.4.31:5000/graphql"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <Employee></Employee>
+      </div>
+    </ApolloProvider>
   );
 }
 
